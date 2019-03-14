614980b2d455a96ea046b071c0fd4a471a5550fc
Michael Robertson <mrobertson00@stuy.edu>
2019-03-14 13:34:45 -0400

    edited test

--- UserOfList.java
+++ UserOfList.java
@@ -5,8 +5,11 @@ public class UserOfList {
     public static void main( String[] args ) {
         List_inArraySlots list = new List_inArraySlots();
 
-        list.add(1);
-	list.add(3.14);
+	int one = 1;
+	double pi = 3.14;
+
+        list.add(integer);
+	list.add(twice);
 	list.add("hello");
 	list.add(true);
 
